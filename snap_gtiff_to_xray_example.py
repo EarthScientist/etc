76140b641350319b652d3dc1b803870998b309da
EarthScientist <lindgren.mike@gmail.com>
2015-06-28 13:42:51 -0800

    adds script showing potential use of xray with mulitband Gtiffs from the SNAP servers.  work in progress.

---
+++ snap_gtiff_to_xray_example.py
@@ -0,0 +1,62 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# this is a simple testing script to see what I can do with the Xray library and the type of time series data we use 
+# at snap.  This is just a set of simple things I have had success with.
+#
+# Michael Lindgren malindgren@alaska.edu
+#
+# LICENSE: MIT
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
+def coordinates( fn ):
+	'''
+	take a raster file as input and return the centroid coords for each 
+	of the grid cells as a pair of numpy 2d arrays (longitude, latitude)
+	'''
+	import rasterio
+	import numpy as np
+	from affine import Affine
+	from pyproj import Proj, transform
+
+	# Read raster
+	with rasterio.open(fn) as r:
+		T0 = r.affine  # upper-left pixel corner affine transform
+		p1 = Proj(r.crs)
+		A = r.read_band(1)  # pixel values
+
+	# All rows and columns
+	cols, rows = np.meshgrid(np.arange(A.shape[1]), np.arange(A.shape[0]))
+
+	# Get affine transform for pixel centres
+	T1 = T0 * Affine.translation(0.5, 0.5)
+	# Function to convert pixel row/column index (from 0) to easting/northing at centre
+	rc2en = lambda r, c: (c, r) * T1
+
+	# All eastings and northings (there is probably a faster way to do this)
+	eastings, northings = np.vectorize(rc2en, otypes=[np.float, np.float])(rows, cols)
+
+	# Project all longitudes, latitudes
+	longs, lats = transform(p1, p1.to_latlong(), eastings, northings)
+	return longs, lats
+
+if __name_- == '__main__':
+	import os, rasterio, xray
+	import pandas as pd
+	import numpy as np
+
+	# filename
+	fn = '/Users/malindgren/Documents/snap_data_test/tas_mean_C_alf_cru_TS31_07_1901_2009.tif'
+	
+	# read in the data using rasterio to a numpy ndarray
+	data = rasterio.open( fn ).read()
+
+	# lets make a time dimension that matches this particuar data which is all Julys for a period of years
+	times = pd.date_range( '1901', '2010', freq='A-JUL' )
+
+	# get a meshgrid of the input lons and lats.
+	lons, lats = coordinates( fn )
+
+	# the only odd thing here is that there could be some issues with the lats / lons that needs sorting. 
+	# Potentially due to the number of unique lat longs in projected space to what is expected based on the coordinates of the numpy ndarrays....
+	new_arr = xray.DataArray( data, coords=( times, lons[:, 1],lats[1,:]), dims=('time', 'lon', 'lat'), name=None, attrs=None )
+
+	# now we can slice by the 
