5711bbb18ed4bf8853078d20c9a34d47e2315143
EarthScientist <lindgren.mike@gmail.com>
2017-06-27 22:29:11 -0800

    changes to grab the first (and only) feature from each layer in the gdb and puts in a GeoDataFrame

--- gdb_listlayers_intersect_with_extent.py
+++ gdb_listlayers_intersect_with_extent.py
@@ -1,30 +1,38 @@
 # * * * * * * * * * * * * * * * * * * 
 # ABOVE FOOTPRINT INTERSECT
 # * * * * * * * * * * * * * * * * * * 
+def open_layer( fn, layer ):
+	''' opens the gdb layer and returns the first row '''
+	with fiona.open( gdb_fn, layer=layername ) as c:
+		dat = next( c )
+	return dat
 
-from shapely.geometry import Polygon, LineString
-import fiona, os, itertools
-import geopandas as gpd
+if __name__ == '__main__':
+	from shapely.geometry import Polygon, LineString
+	import fiona, os, itertools
+	import geopandas as gpd
 
-gdb_fn = '/Users/malindgren/Documents/repos/prajna/RH_SampleData.gdb'
-extent_fn = '/Users/malindgren/Documents/repos/prajna/extent.shp' # psuedo
+	gdb_fn = '/Users/malindgren/Documents/repos/prajna/RH_SampleData.gdb'
+	extent_fn = '/Users/malindgren/Documents/repos/prajna/extent.shp' # psuedo
 
-# open the shapefile extent and get the geometry of the extent polygon
-ext_df = gpd.read_file( extent_fn )
-ext = ext_df.iloc[0][ 'geometry' ] # assumes first row in df is extent poly
+	# open the shapefile extent and get the geometry of the extent polygon
+	ext_df = gpd.read_file( extent_fn )
+	ext = ext_df.iloc[0][ 'geometry' ] # assumes first row in df is extent poly
 
-# get the layernames from the gdb
-layernames = fiona.listlayers( gdb_fn )
-layername = layernames[ 1 ]
+	# get the layernames from the gdb
+	layernames = fiona.listlayers( gdb_fn )
 
-# make a layer into the geodataframe
-gdf = gpd.read_file( fn, layer=layername )
+	# get the first row from each layer...
+	gdf = gpd.GeoDataFrame.from_features([ open_layer( fn, layername ) for layername in layernames[1:3] ])
 
-# test the footprint geometries to your AOI geometry and return True / False
-gdf[ 'intersects' ] = [ ext.intersects( geom ) for geom in gdf.geometry ]
+	# # make a layer into the geodataframe
+	# gdf = gpd.read_file( fn, layer=layername )
 
-# select all the rows that intersect your data
-gdf_intersect_aoi = gdf[ gdf['intersects'] == True ]
+	# test the footprint geometries to your AOI geometry and return True / False
+	gdf[ 'intersects' ] = [ ext.intersects( geom ) for geom in gdf.geometry ]
+
+	# select all the rows that intersect your data
+	gdf_intersect_aoi = gdf[ gdf['intersects'] == True ]
 
 
 # * * * * * * OLD ACCESS METHOD * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
