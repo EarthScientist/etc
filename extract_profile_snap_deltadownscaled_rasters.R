720c441322ba54f81020ee710fd24d8f0e0fd8c5
EarthScientist <lindgren.mike@gmail.com>
2019-01-14 15:59:46 -0900

    adds way to do extraction by making SPDF with a CSV of lon/lat

--- extract_profile_snap_deltadownscaled_rasters.R
+++ extract_profile_snap_deltadownscaled_rasters.R
@@ -6,11 +6,16 @@ setwd("./working")
 
 # set a folder location to where the files are stored on your system. 
 # in this case I am going into the CRU historical precip data
-input_path <- "./downscaled/CRU-TS40/historical/pr"
+input_path <- "./downscaled/CRU-TS40/historical/pr/"
 
 # point to the point shapefile you want to use.
 # [note]: you may have to reproject these points to the same reference system as the SNAP data (EPSG:3338)
-pointSPDF <- readOGR("./shapefile/points.shp")
+# pointSPDF <- readOGR("./shapefile/points.shp")
+
+# try with a CSV and make an SPDF from it
+csv <- read.csv( './kenai_places.csv', sep=';' ) # it has a semicolon separator
+pointSPDF <- SpatialPointsDataFrame( coords=SpatialPoints(csv[c(2,3)]), data=csv[1] )
+projection(pointSPDF) <- CRS("+init=epsg:4326") # set the projection
 
 # [NOTE]: for reprojection I would use the spTransform function from the rgdal package. A look at the man file with ?spTransform will show 
 #  ways to implement the method -- try it first without the reprojection as the function 'extract' might handle that for you if there is a legit
@@ -35,10 +40,13 @@ for(y in years){
 }
 
 # read the files we just listed as a raster stack
-s <- stack(list.2km)
+s <- stack(list.2km[1:100])
+
+# extract the points from the stack. (it reprojects on the fly)
 extraction <- extract(s, pointSPDF)
 
 # then transpose it so the point locs are in the cols, and the samples are in the rows
 extraction <- t(extraction)
 
 # from here you should be able to dump these to CSV or work with the values in subsequent R functions.
+write.csv( extraction, file='./extracted_points.csv' )
